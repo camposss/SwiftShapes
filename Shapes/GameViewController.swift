03972379016d89f092063b303d037bcf2ce6d1c5
camposss <ccamposs23@gmail.com>
2018-12-04 21:19:29 -0800

    Initial Commit

---
+++ Shapes/GameViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let scene = GameScene(size: CGSize(width: 1536, height: 2048))
+    let skView = self.view as! SKView
+    skView.showsFPS = true
+    skView.showsNodeCount = true
+    skView.ignoresSiblingOrder = true
+    scene.scaleMode = .aspectFill
+    skView.presentScene(scene)
+  }
+  
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
+}
